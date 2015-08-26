c39adf131cb30f87a9a77c783ea8ad63ab7eea09
Simon Steinaa <me@simsteinaa.dk>
2015-08-26 11:20:19 +0200

    Refactored and solved the exercise

--- Threads-2_ex2FibonacciObserver/src/fibonacciobserver/FibonacciTask.java
+++ Threads-2_ex2FibonacciObserver/src/fibonacciobserver/FibonacciTask.java
@@ -15,12 +15,26 @@ public class FibonacciTask extends Thread{
     public FibonacciTask(long n) {
         this.tal = n;
     }
+    
+    private long fib(long n) {
+    if ((n == 0) || (n == 1)) {
+      return n;
+    } else {
+      return fib(n - 1) + fib(n - 2);
+    }
+  }
+
     @Override
     public void run() {
         //Call the Fibonacci method from here
-        //long tal = ......
+        long talResult = fib(tal);
+        
+        //System.out.println("" + talResult);
+        
+        
+        
         for(FibonacciObserver observer : observers){
-          observer.dataReady(tal);
+          observer.dataReady(talResult);
         }
     }
 }
