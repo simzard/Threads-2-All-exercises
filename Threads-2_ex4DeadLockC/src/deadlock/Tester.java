2310d295e78717d680092d3edf765130ed3e4e70
Simon Steinaa <me@simsteinaa.dk>
2015-08-26 14:04:54 +0200

    added ex4 c implementation

--- Threads-2_ex4DeadLockC/src/deadlock/Tester.java
+++ Threads-2_ex4DeadLockC/src/deadlock/Tester.java
@@ -6,11 +6,10 @@ import java.util.logging.Logger;
 public class Tester {
 
     public static void main(String[] args) {
+        Thread monitor = new Thread(new DeadLockDetector());
+        monitor.start();
         try {
 
-            Thread monitor = new Thread(new DeadLockDetector());
-            monitor.start();
-            
             ResourceContainer resources = new ResourceContainer();
             ResourceUser1 t1 = new ResourceUser1(resources);
             ResourceUser2 t2 = new ResourceUser2(resources);
@@ -23,8 +22,10 @@ public class Tester {
             System.out.println("Done");
             System.out.println("Words produced: " + resources.getResourceWords().size());
             System.out.println("Numbers produced: " + resources.getResourceNumbers().size());
+            monitor.join(); // an attempt to stop the monitor thread - it is not working however!
         } catch (InterruptedException ex) {
             Logger.getLogger(Tester.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 }
